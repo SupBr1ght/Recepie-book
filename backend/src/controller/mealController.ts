32b259c6a216f98f6643fffdaef062b499881bc1
Твоє numbmn2n <pechkursasha@gmail.com>
2025-03-01 14:26:48 +0200

    feat: add meal service and filter it by country

--- backend/src/controller/mealController.ts
+++ backend/src/controller/mealController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from "express";
+import { MealService } from "../services/mealService";
 export class MealController{
     static async getMeals(req: Request, res: Response): Promise<void> {
         try {
-            console.log(`Controller get request: ${req.url} `); // our request 
-            console.log('This is our query parameters')
+            console.log(`Controller get request: ${req.url} 🤖`); // our request 
+            console.log('This is our query parameters ✍️')
 
             const filters = req.query; // this is our request query params
 
-            const meals  = await MealService.fetchMeals(filters) // get our query params to the server
+            const meals  = await MealService.fetchMeals(filters) // push our query params to the server
             res.json(meals)
         } catch (error) {
-            res.status(500).json({ error: "Failed to fetch meals" });
+            res.status(500).json({ error: "Failed to fetch meals ❌" });
         }
     }
 }
---
+++ backend/src/services/mealService.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export class MealService {
+    static async fetchMeals(filters: { country?: string; ingredient?: string }): Promise<any> {
+        console.log("Service get filters 😍");
+
+        const { country, ingredient } = filters; // destructurize our object
+        let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" //all our meals
+
+        if (country) {
+            url = `www.themealdb.com/api/json/v1/1/filter.php?a=${country}`
+            console.log('Filter by country: ', country);
+        }
+
+        console.log("🌐 processing API request:", url);
+        const response = await axios.get(url);
+        console.log(`Our response look like ${response}`);
+        console.log(`Our response data look like ${response.data}`);
+        return response.data.meals || [];
+
+    }
+}
